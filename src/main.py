f3ee6c860061b5e1ada666beebb874c58c70be70
kei-harada <kei.harada@dena.jp>
2020-04-26 13:11:42 +0900

    work0426-3

--- src/main.py
+++ src/main.py
@@ -14,13 +14,19 @@ def main(dir_path, time_limit):
 
         if file_name[-5:] == ".json":
             data = json.load(open(dir_path + file_name, "r"))
-            solved_dict = auto_solve(data, time_limit=time_limit)
+            # print(len(data["test"]))
+            try:
+                solved_dict = auto_solve(data, time_limit=time_limit)
 
-            for j in solved_dict.keys():
-                assert len(solved_dict[j]) <= 3
+                for j in solved_dict.keys():
+                    assert len(solved_dict[j]) <= 3
 
-                output_id_list.append(f'{file_name[:-5]}_{j}')
-                output_list.append(" ".join(solved_dict[j]))
+                    output_id_list.append(f'{file_name[:-5]}_{j}')
+                    output_list.append(" ".join(solved_dict[j]))
+            except:
+                for j in range(len(data["test"])):
+                    output_id_list.append(f'{file_name[:-5]}_{j}')
+                    output_list.append("")
 
     res_df = pd.DataFrame({"output_id": output_id_list, "output": output_list})
     # print(res_df)
